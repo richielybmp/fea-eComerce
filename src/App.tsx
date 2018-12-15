5151b25e044a15cf7cacf2de65ee43f0e4cb9446
Richiely Paiva <richielybmp@hotmail.com>
2018-12-15 11:39:53 -0200

    Commit inicial.

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Card, Icon, Image } from 'semantic-ui-react'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          Olá eComerce!
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default App;

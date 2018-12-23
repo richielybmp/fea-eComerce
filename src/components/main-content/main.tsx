31029d9061b3553251978231c2ba0cd5fdac4ace
Richiely Paiva <richielybmp@hotmail.com>
2018-12-23 12:50:27 -0200

    Configurando GitHub Pages

--- src/components/main-content/main.tsx
+++ src/components/main-content/main.tsx
@@ -8,7 +8,7 @@ export default function MainContents() {
     return (
         <main className={'container-main pusher'}>
             <Switch>
-                <Route exact path='/' component={ProductContainer} />
+                <Route exact path={["/", "/fea-eCommerce"]} component={ProductContainer} />
                 <Route path='/carrinho' component={CarrinhoContainer} />
                 <Route path='*' component={NotFound} />
             </Switch>

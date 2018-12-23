e64dae34a5fa17e06564e17fb9683db3f16acbc6
Richiely Paiva <richielybmp@hotmail.com>
2018-12-23 12:38:47 -0200

    Updates

---
+++ precache-manifest.717092d228de456f9ca2defbcd89fc7b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9585df1e321d02c9412d",
+    "url": "/fea-eCommerce/static/js/runtime~main.9585df1e.js"
+  },
+  {
+    "revision": "88f5e821d18e34ff2aa3",
+    "url": "/fea-eCommerce/static/js/main.88f5e821.chunk.js"
+  },
+  {
+    "revision": "ac232e2d2dcfb1e6a1b5",
+    "url": "/fea-eCommerce/static/js/1.ac232e2d.chunk.js"
+  },
+  {
+    "revision": "88f5e821d18e34ff2aa3",
+    "url": "/fea-eCommerce/static/css/main.e3b4a98e.chunk.css"
+  },
+  {
+    "revision": "aeed230bb5698170ae7525204cc3d090",
+    "url": "/fea-eCommerce/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/fea-eCommerce/precache-manifest.717092d228de456f9ca2defbcd89fc7b.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/fea-eCommerce/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});

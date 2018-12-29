06d4126d3313fa5e42b5a76e6c375c1bfeaa91a4
Mateus Stedler <mstedlerr@gmail.com>
2018-12-29 17:40:07 -0200

    Updates

---
+++ precache-manifest.b6504422b526f2a877981c26d31c123c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9585df1e321d02c9412d",
+    "url": "/fea-eCommerce/static/js/runtime~main.9585df1e.js"
+  },
+  {
+    "revision": "aa7a149239aab336be84",
+    "url": "/fea-eCommerce/static/js/main.aa7a1492.chunk.js"
+  },
+  {
+    "revision": "014ddd8cb68d75e28acb",
+    "url": "/fea-eCommerce/static/js/1.014ddd8c.chunk.js"
+  },
+  {
+    "revision": "aa7a149239aab336be84",
+    "url": "/fea-eCommerce/static/css/main.88d1c307.chunk.css"
+  },
+  {
+    "revision": "710b8977c3006c38f485b356a6be7a42",
+    "url": "/fea-eCommerce/index.html"
+  }
+];

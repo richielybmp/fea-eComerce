71f1b66d824a7931c4f078ccd6f2d30aeb8abb18
richielybmp <richielybmp@hotmail.com>
2018-12-29 17:36:35 -0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/fea-eCommerce/precache-manifest.3f566356bd974c66892b0732feb35865.js"
+  "/fea-eCommerce/precache-manifest.b123170fafde242c6b89a1d8f744f9ea.js"
 );
 
 workbox.clientsClaim();

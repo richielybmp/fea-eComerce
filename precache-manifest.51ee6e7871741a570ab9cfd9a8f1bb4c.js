2a434d945c170825b8c60f895a77ec1a7b975ee3
richielybmp <richielybmp@hotmail.com>
2018-12-30 15:08:15 -0200

    Updates

--- precache-manifest.51ee6e7871741a570ab9cfd9a8f1bb4c.js
+++ precache-manifest.51ee6e7871741a570ab9cfd9a8f1bb4c.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/fea-eCommerce/static/js/runtime~main.9585df1e.js"
   },
   {
-    "revision": "5375d24e93fd81972683",
-    "url": "/fea-eCommerce/static/js/main.5375d24e.chunk.js"
+    "revision": "acb4cd4f095f8389ba2c",
+    "url": "/fea-eCommerce/static/js/main.acb4cd4f.chunk.js"
   },
   {
     "revision": "e400cefa204a36c36a1c",
     "url": "/fea-eCommerce/static/js/1.e400cefa.chunk.js"
   },
   {
-    "revision": "5375d24e93fd81972683",
+    "revision": "acb4cd4f095f8389ba2c",
     "url": "/fea-eCommerce/static/css/main.a1cabcfd.chunk.css"
   },
   {
-    "revision": "68fb670ee14da4a6c685e4db8a9bdc0f",
+    "revision": "3251a9199a27cb2a1615e26e2b42ba3d",
     "url": "/fea-eCommerce/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/fea-eCommerce/precache-manifest.a876fd6aa76e791e411b27b5a90b2a29.js"
+  "/fea-eCommerce/precache-manifest.51ee6e7871741a570ab9cfd9a8f1bb4c.js"
 );
 
 workbox.clientsClaim();
